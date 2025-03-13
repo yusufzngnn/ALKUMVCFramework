7d1879a039defc4e11bb138c02cf866e8112e368
yusufzngnn <232011045@ogr.alanya.edu.tr>
2025-03-13 14:34:41 +0300

    13.03son

--- controllers/indexController.php
+++ controllers/indexController.php
@@ -1,7 +1,7 @@
 <?php
 
 Router::layout('_main', function (){
-    Router::get('', fn () => View::render('index/home', "ALKUMVC Ana Sayfa"));
+    Router::get('', fn () => View::render('index/home', ['pageTitle' => 'Anasayfa']));
     Router::get('hakkimizda', fn () => View::render('index/about', "Hakkımızda"));
     Router::get('iletisim', fn () => View::render('index/contact', "İletişim"));
     Router::get('berat', fn () => View::render('index/beratogrenmekistiyor', ">Beberat"));
--- views/index/beratogrenmekistiyor.php
+++ views/index/beratogrenmekistiyor.php
@@ -1,2 +1,1 @@
 <h1>Berat Öğrenmek İstiyor.</h1>
-    
