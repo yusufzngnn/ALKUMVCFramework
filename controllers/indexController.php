33b797634db5559719f7ffee9ae8b4e1e13a73b1
yusufzngnn <232011045@ogr.alanya.edu.tr>
2025-03-20 09:58:33 +0300

    Update indexController.php

--- controllers/indexController.php
+++ controllers/indexController.php
@@ -7,3 +7,46 @@ Router::layout('_main', function (){
     Router::get('berat', fn () => View::render('index/beratogrenmekistiyor', ">Beberat"));
 
 });
+
+
+
+
+/* HTTP istek türleri. |||BUNLARI SİLMEYECEĞİM /// BUNLARI SİLMEYECEĞİM|||
+ *  GET: Sadece veri almak için kullanılır.
+ *  POST: Veri göndermek için kullanılır.
+ *  PUT: Veri güncellemek için kullanılır.
+ *  DELETE: Veri silmek için kullanılır.
+ *
+ *
+ *  PATCH: Veri güncellemek için kullanılır. PUT ile benzerdir, ancak yalnızca belirli alanları günceller.
+ *
+ *  OPTIONS: Sunucu üzerindeki kaynaklar hakkında bilgi almak için kullanılır.
+ *  HEAD: GET isteğine benzer, ancak yanıtın gövdesini almaz.
+ *
+ *  CONNECT: Sunucu ile proxy arasında bağlantı kurmak için kullanılır.
+ *  TRACE: Sunucuya bir mesaj gönderir ve geri dönen mesajın orijinal isteği nasıl etkilediğini görmek için kullanılır.
+ *  COPY: Bir kaynağı kopyalamak için kullanılır.
+ *  LINK: Bir kaynağı başka bir kaynağa bağlamak için kullanılır.
+ *  UNLINK: Bir kaynağı başka bir kaynaktan ayırmak için kullanılır.
+ *  PURGE: Bir web önbelleğinden belirli bir kaynağı kaldırmak için kullanılır.
+ * */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
